9be0f92bac5ebefc3200eb9c88cc67e91a6981fe
JD Wilson <thejdwilson@gmail.com>
2019-12-19 10:59:14 -0500

    initial commit
    Contains the programs I typically open up when I start my day. Needs to be placed in "C:\Users\[user]\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"

---
+++ startupPrograms.cmd
@@ -0,0 +1,9 @@
+start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe"
+start "" C:\ConsoleZ\Console.exe
+start "" "C:\Program Files\Git\git-bash.exe"
+start "" "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.3.4\bin\idea64.exe"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "C:\Program Files\Notepad++\notepad++.exe"
+start "" C:\Users\jdwilson\AppData\Local\Microsoft\Teams\current\Teams.exe
+start "" "C:\Users\jdwilson\AppData\Local\Programs\Microsoft VS Code\bin\Code.cmd"
+cd C:\Users\jdwilson\Documents
