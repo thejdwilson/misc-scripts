06e95872707bc9dfea32b4fa648c2bfe82ef6f05
JD Wilson <thejdwilson@gmail.com>
2020-06-17 09:55:07 -0400

    Update startup script
    Including comments on how to find the startup folder. Minor changes to default programs.

--- startupPrograms.cmd
+++ startupPrograms.cmd
@@ -1,9 +1,13 @@
-start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe"
+# Place this script in the startup folder --> Windows+R, then type shell:startup
+
+#start "" dbisql
 start "" C:\ConsoleZ\Console.exe
+start "" C:\Program Files\DBeaver\dbeaver.exe
+start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe"
 start "" "C:\Program Files\Git\git-bash.exe"
 start "" "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.3.4\bin\idea64.exe"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 start "" "C:\Program Files\Notepad++\notepad++.exe"
 start "" C:\Users\jdwilson\AppData\Local\Microsoft\Teams\current\Teams.exe
 start "" "C:\Users\jdwilson\AppData\Local\Programs\Microsoft VS Code\bin\Code.cmd"
-cd C:\Users\jdwilson\Documents
+exit
