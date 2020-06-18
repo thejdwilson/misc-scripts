1d6537b6e6dc8134b87fbb4d87b7a8973ea7350d
JD Wilson <thejdwilson@gmail.com>
2020-06-18 07:25:33 -0400

    Forgot quotes on line 5

--- startupPrograms.cmd
+++ startupPrograms.cmd
@@ -2,7 +2,7 @@
 
 #start "" dbisql
 start "" C:\ConsoleZ\Console.exe
-start "" C:\Program Files\DBeaver\dbeaver.exe
+start "" "C:\Program Files\DBeaver\dbeaver.exe"
 start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe"
 start "" "C:\Program Files\Git\git-bash.exe"
 start "" "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.3.4\bin\idea64.exe"
